82076231d7bcc84c09a38fee5cebb30703d07550
Ibrahim Sangaré <1234567+isangare-ux@users.noreply.github.com>
2026-05-27 19:05:01 +0200

    systeminfo.sh aus feature/dateipruefung entfernt

--- scripts/systeminfo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd ..
-echo "-----------------------Systeminformationen-------------------------"
-echo "Benutzername: $(whoami)"
-echo "Hostname: $(hostname)"
-echo "Datum und Uhrzeit:   $(date +"%d.%m.%Y %T")"
-echo "Verzeichnis:  $(pwd)"
